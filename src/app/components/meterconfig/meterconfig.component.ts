9713f308f8cbc950a820e30c8e3223c95dfbab74
fredrjo <fredrik.johnsen@cebyc.no>
2018-10-09 23:14:45 +0200

    layout improvements

--- src/app/components/meterconfig/meterconfig.component.ts
+++ src/app/components/meterconfig/meterconfig.component.ts
@@ -10,12 +10,15 @@ import { ConnectService } from '../../services/connect.service';
 export class MeterconfigComponent implements OnInit {
 
     private meters = [];
+    private meter;
+    private task_id = 0;
   constructor(private route: ActivatedRoute,private conn: ConnectService) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.paramMap.get('id');
     this.conn.getResource('/meter/' + id).subscribe(res => {
-      console.log(res);
+      this.meter = res.meter;
+      this.task_id = res.meter.task_id;
     });
   }
 
