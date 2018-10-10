5fd9a55a83f49197798ac04c9daf2f289aca0253
fredrjo <fredrik.johnsen@cebyc.no>
2018-10-10 23:14:07 +0200

    layout improvements

--- src/app/components/meterconfig/meterconfig.component.ts
+++ src/app/components/meterconfig/meterconfig.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ConnectService } from '../../services/connect.service';
 
 @Component({
@@ -12,14 +12,26 @@ export class MeterconfigComponent implements OnInit {
     private meters = [];
     private meter;
     private task_id = 0;
-  constructor(private route: ActivatedRoute,private conn: ConnectService) { }
+    private meter_id = '';
+    private extras = '';
+    private import_id = '';
+    private disabled = false;
+  constructor(private route: ActivatedRoute, private conn: ConnectService, private router: Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.paramMap.get('id');
     this.conn.getResource('/meter/' + id).subscribe(res => {
-      this.meter = res.meter;
-      this.task_id = res.meter.task_id;
+      console.log(res);
+      this.meter = res;
+      this.task_id = res.task_id;
+      this.meter_id = res.meter_id;
+      this.extras = res.export;
+      this.disabled = res.disabled;
+      this.import_id = res.import_id;
     });
+  }
+  goToTask() {
+    this.router.navigate(['/tasksetup/' + this.task_id]);
   }
 
 }
